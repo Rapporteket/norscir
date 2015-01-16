0b77221b094ee08e8f8f93f3b8a57071ee51c03a
Are Edvardsen <are.edvardsen@helse-nord.no>
2015-01-16 14:26:32 +0100

    first commit

---
+++ R/NSFigAndelStabel.R
@@ -0,0 +1,185 @@
+FigAndelStabel <- function(RegData, outfile='', libkat, valgtVar, datoFra='2010-01-01', datoTil='3000-01-01', 
+		minald=0, maxald=120, erMann='', traume='', egenavd=1 , reshID)
+{
+#Søylediagram som sammenligner fordeling (vha. stablede søyler) av valgt variabel 
+#hos fra to ulike utvalg, f.eks. eget/resten, før/etter
+#(Kan velge å ikke ha med sammenligninga)
+
+#Søylediagram som viser andeler av valgt variabel: 
+#'NevrNivaaInn' - Nevrologisk nivå ved innskrivelse, sml. eget, andre
+#'NevrNivaaUt' - Nevrologisk nivå ved utskrivelse, sml. eget, andre
+#'NevrNivaaInnUt' - sml. Nevrologisk nivå ved inn- og utskrivelse
+
+#Hvilke kombinasjonsmuligheter har vi?
+#Inn / Ut (Velge: bare eget/hele landet)
+#Eget / resten av landet (Velge: Inn eller Ut)
+
+
+#Inngangsdata: 	
+#	RegData - ei dataramme med alle nødvendige variable fra registeret
+#	libkat - sti til bibliotekkatalog
+#   outfile - navn på fil figuren skrives ned til
+#	reshID - avdelingsid for egen avdeling, må angis
+# 	Brukerstyrt i Jasper:
+#		valgtVar - Må velges: ... NevrNivaaInnUt, NevrNivaaInn, NevrNivaaUt 
+#		egenavd - 1:eget sykehus, 0:hele landet (standard) Kun for valgtVar=='NevrNivaaInnUt'
+#		erMann - kjønn, 1-menn, 0-kvinner, standard: '' (alt annet enn 0 og 1), dvs. begge
+#		minald - alder, fra og med
+#		maxald - alder, til og med
+#		traume - 'ja','nei', standard: ikke valgt
+#		datoFra <- '2010-01-01'    # min og max dato i utvalget vises alltid i figuren.
+#		datoTil <- '2013-05-25'
+
+
+#Trenger funksjonene...:? 
+source(paste(libkat, 'LibFigFilType.R', sep=''), encoding="UTF-8")
+source(paste(libkat, 'NSLibUtvalg.R', sep=''), encoding="UTF-8")
+
+#------------Gjøre utvalg-------------------------
+#Definerer funksjonsspesifikke variable................
+
+if (valgtVar %in% c('NevrNivaaInn','NevrNivaaUt')) {
+	RegData$Variabel <- as.numeric(RegData[ ,valgtVar])} else {
+	 RegData$Variabel <- RegData$NevrNivaaInn-RegData$NevrNivaaUt}	#Vil bare ha pasienter som har reg. både inn og ut.
+	
+#Gjør utvalg (Manglende data i variablene tas høyde for i variabeldef.)
+Utvalg <- NSLibUtvalg(RegData=RegData, datoFra=datoFra, datoTil=datoTil, minald=minald, maxald=maxald, 
+		erMann=erMann, traume=traume, AIS='')
+RegData <- Utvalg$RegData
+utvalgTxt <- Utvalg$utvalgTxt
+
+cexgr <- 0.9
+grtxt <- ''
+grtxt2 <- ''
+
+
+#Sml eget/resten av landet.
+if (valgtVar %in% c('NevrNivaaInn','NevrNivaaUt')) {
+#	if (reshID != 0) {ShNavn <- '' 
+#					Andre <- 'Alle'}
+#	if (reshID %in% c(105593, 106896, 107627)) {
+	ShNavn <- as.character(RegData$ShNavn[which(RegData$ReshId == reshID)[1]])
+	Andre <- 'Andre' #}
+
+#	NRest <- length(ind$Rest)
+#	NSh <- length(ind$Sh)
+	RegData$AIS <- switch(valgtVar,
+						NevrNivaaInn = as.character(RegData$AAis),
+						NevrNivaaUt = as.character(RegData$FAis))
+	#Slå sm D og E
+	RegData <- RegData[RegData$AIS %in% LETTERS[1:5], ]	#Bare de med kategori A:E, ikke U
+	RegData$AIS[which(RegData$AIS %in% c('D', 'E'))] <- 'D+E'
+	ind <- list(Sh=which(RegData$ReshId == reshID), 
+				Rest=which(RegData$ReshId != reshID))
+	#Alternativt bruke to datasett?
+	RegDataLand <- RegData
+	tittel <- switch(valgtVar,
+		NevrNivaaInn = 'Nevrologisk kategori ved innleggelse',
+		NevrNivaaUt = 'Nevrologisk kategori ved utskriving')
+	subtxt <- 'Nevrologisk nivå'
+	grtxt1 <- c(ShNavn, Andre)
+	grtxt2 <- c('C1-4', 'C5-8', 'T,L,S')
+	Skala <- c('A','B','C','D+E')
+	Ngr <- cbind(
+		table(factor(RegData$AIS)[intersect(ind$Sh, which(RegData$Variabel %in% 1:4))]), 	#C0104Sh
+		table(factor(RegData$AIS)[intersect(ind$Rest, which(RegData$Variabel %in% 1:4))]),	#C0104Rest
+		table(factor(RegData$AIS)[intersect(ind$Sh, which(RegData$Variabel %in% 5:8))]),	#C0508Sh
+		table(factor(RegData$AIS)[intersect(ind$Rest, which(RegData$Variabel %in% 5:8))]),	#C0508Rest
+		table(factor(RegData$AIS)[intersect(ind$Sh, which(RegData$Variabel %in% 9:30))]),	#TLSSh
+		table(factor(RegData$AIS)[intersect(ind$Rest, which(RegData$Variabel %in% 9:30))]))	#TLSRest
+}
+
+
+if (valgtVar == 'NevrNivaaInnUt') {
+	#RegData$GrVar1 <- RegData$NevrNivaaInn
+	#RegData$GrVar2 <- RegData$NevrNivaaUt
+	#Tar bort de som ikke har fått AIS-nivå el nivå U:
+	RegData <- RegData[RegData$AAis %in% LETTERS[1:5], ]	#Bare de med kategori A:E, ikke U
+	RegData <- RegData[RegData$FAis %in% LETTERS[1:5], ]
+	RegData$Var1 <- as.character(RegData$AAis)
+	RegData$Var2 <- as.character(RegData$FAis)
+	#Slå sm D og E
+	RegData$Var1[which(RegData$Var1 %in% c('D', 'E'))] <- 'D+E'
+	RegData$Var2[which(RegData$Var2 %in% c('D', 'E'))] <- 'D+E'
+	if (egenavd==1) {
+		ind <- which(RegData$ReshId == reshID)
+		RegData <- RegData[ind,]
+		ShNavn <- as.character(RegData$ShNavn[1])} else {ShNavn <- 'Alle sykehus'}
+	tittel <- c('Nevrologisk kategori ved inn- og utskriving,', ShNavn)
+	subtxt <- 'Nevrologisk nivå'
+	grtxt1 <- rep(c('Inn','Ut'))
+	grtxt2 <- c('C1-4', 'C5-8', 'T,L,S')
+	Skala <- c('A','B','C','D+E')
+	RegData$Var1 <- factor(RegData$Var1, levels=c(LETTERS[1:3],'D+E'))
+	RegData$Var2 <- factor(RegData$Var2, levels=c(LETTERS[1:3],'D+E'))
+	#GrVar1 = NevrNivaaInn, Var=AAis
+		indGr1 <- intersect(which(RegData$NevrNivaaInn %in% 1:4), which(RegData$NevrNivaaUt %in% 1:4))
+		indGr2 <- intersect(which(RegData$NevrNivaaInn %in% 5:8), which(RegData$NevrNivaaUt %in% 5:8))
+		indGr3 <- intersect(which(RegData$NevrNivaaInn %in% 9:30), which(RegData$NevrNivaaUt %in% 9:30))
+	Ngr <- cbind(
+		table(RegData$Var1[indGr1]), 	#C0104inn
+		table(RegData$Var2[indGr1]),	#C0104ut
+		table(RegData$Var1[indGr2]),	#C0508inn
+		table(RegData$Var2[indGr2]),	#C0508ut
+		table(RegData$Var1[indGr3]),	#TLSinn
+		table(RegData$Var2[indGr3]))	#TLSut
+}
+#Definere grupperingsvariabel med gyldige grupper i utgangspunktet i stedet for å gjøre det i indGr?
+
+
+#------------------------- Beregninger -----------------------------------------
+#utvalg <- c('Sh', 'Rest') #som Var1 og Var 2
+#Andeler <- list(Sh = 0, Rest =0)
+#ind_sh <-which(as.numeric(RegData$AvdReshID)==egenReshID) 	
+#ind_resten <- which(as.numeric(RegData$AvdReshID)!=egenReshID)
+#Ngr <- cbind('Egen avdeling'=table(RegData[ind_sh, variabel]), 
+#			'Landet forøvrig'= table(RegData[ind_resten, variabel]))
+N <- colSums(Ngr)
+AndelStabel <- round(prop.table(Ngr,2)*100, 1)
+
+
+if (dim(RegData)[1]==0)	{# | (NSh==0 & egenavd==1)) {
+	#-----------Figur---------------------------------------
+figtype(outfile)
+	plot.new()
+	title(main=tittel)
+	text(0.5, 0.7, 'Ingen egne data registrert for dette utvalget',cex=1.3)
+	if ( outfile != '') {dev.off()}
+} else {
+
+#-----------Figur---------------------------------------
+#Inn parametre: subtxt, grtxt1, grtxt2, tittel, libkat, AndelStabel
+
+#Plottspesifikke parametre:
+FigTypUt <- figtype(outfile, fargepalett=Utvalg$fargepalett)
+farger <- FigTypUt$farger
+
+par('fig'=c(0, 1, 0, 0.9))
+pos <- barplot(AndelStabel, space=c(0,rep(c(0.1,1),2),0.1), col=farger, border=NA, xlim=c(0,10),
+		ylim=c(0,110), names.arg=NULL, ylab="Andel (%)")
+legend('right', rev(Skala), bty='n', ncol=1, cex=1, xjust=0, fill=rev(farger), border=rev(farger))	#, title=tLeg)
+
+if ((valgtVar %in% c('NevrNivaaInn','NevrNivaaUt')) & (reshID == 0)) {
+	N[c(1,3,5)]=''
+	text(pos, 102.6, paste0(c('','N='), N), cex=0.9)} else {
+	text(pos, 102.6, paste('N=', N, sep=''), cex=0.9)}	 #Antall over søyla
+mtext(at=pos, grtxt1, side=1, las=1, cex=0.8, adj=0.5, line=0.3)	#Tekst under søylene
+mtext(at=c(1, 4.1, 7.2), grtxt2, side=1, cex=0.9, adj=0.5, line=1.3)	
+mtext(subtxt, side=1, cex=1, adj=0.5, line=2.7)	
+
+utvpos <- 5.2
+avst <- 0.8
+krymp <- 0.8
+title(tittel, line=1, font.main=1)
+mtext(utvalgTxt[1], side=3, las=1, cex=krymp, adj=0, line=utvpos, col=farger[1])
+mtext(utvalgTxt[2], side=3, las=1, cex=krymp, adj=0, line=utvpos-avst, col=farger[1])
+mtext(utvalgTxt[3], side=3, las=1, cex=krymp, adj=0, line=utvpos-2*avst, col=farger[1])
+mtext(utvalgTxt[4], side=3, las=1, cex=krymp, adj=0, line=utvpos-3*avst, col=farger[1])
+mtext(utvalgTxt[5], side=3, las=1, cex=krymp, adj=0, line=utvpos-4*avst, col=farger[1])
+
+par('fig'=c(0, 1, 0, 1)) 
+if ( outfile != '') {dev.off()}
+#------------------------------------------------------------------------------
+
+}	#end if
+}	#end function
---
+++ R/NSFigMeanMed.R
@@ -0,0 +1,170 @@
+FigMeanMed <- function(RegData, valgtVar, valgtMaal='Gjsn', datoFra='01-01-2010', datoTil='31-12-2050', 
+		AIS='', minald=0, maxald=130, erMann='', traume='', libkat, outfile='') {
+#Funksjon som genererer en figur med gjennomsnitt/median
+#for hvert sykehus og kan ta inn ulike numeriske variable.
+#Funksjonen er delvis skrevet for å kunne brukes til andre grupperingsvariable enn sykehus
+
+#Inngangsdata: 	
+#	RegData - ei dataramme med alle nødvendige variable fra registeret
+#	libkat - sti til bibliotekkatalog
+#   outfile - navn på fil figuren skrives ned til
+# 	Brukerstyrt i Jasper:
+#		valgtVar - Må velges: ... Alder, DagerRehab, DagerTilRehab, OpphTot[HosptlDy], 
+#		erMann - kjønn, 1-menn, 0-kvinner, standard: '' (alt annet enn 0 og 1), dvs. begge
+#		minald - alder, fra og med
+#		maxald - alder, til og med
+#		traume - 'ja','nei', standard: ikke valgt
+#		AIS - AISgrad ved innleggelse alle(''), velge en eller flere fra A,B,C,D,E,U
+#		datoFra <- '2010-01-01'    # min og max dato i utvalget vises alltid i figuren.
+#		datoTil <- '2013-05-25'
+#		valgtMaal - 'Med' = median. Alt annet gir gjennomsnitt
+
+
+source(paste(libkat, 'LibFigFilType.R', sep=''), encoding="UTF-8")
+source(paste(libkat, 'NSLibUtvalg.R', sep=''), encoding="UTF-8")
+
+#Definerer funksjonssperifikke variable................
+grVar <- 'ShNavn'
+RegData[ ,grVar] <- factor(RegData[ ,grVar])	#, labels=c('Haukeland', 'St.Olav', 'Sunnaas'))
+
+if (valgtVar %in% c('DagerRehab', 'DagerTilRehab')) {
+	RegData$Variabel <- RegData[ ,valgtVar] }
+if (valgtVar == 'Alder') {RegData$Variabel <- RegData$AlderAar}
+if (valgtVar == 'OpphTot') {RegData$Variabel <- RegData$HosptlDy}
+if (valgtVar == 'Permisjon') {RegData$Variabel <- RegData$OutOfHosptlDy
+				RegData <- RegData[RegData$OutOfHosptlDy>0,]}	#Bare vits i å se på de som faktisk har permisjon
+
+#Tar ut de med manglende registrering av valgt variabel og gjør utvalg
+Utvalg <- NSLibUtvalg(RegData=RegData, datoFra=datoFra, datoTil=datoTil, minald=minald, maxald=maxald, 
+		erMann=erMann, traume=traume, AIS=AIS)
+RegData <- Utvalg$RegData
+utvalgTxt <- Utvalg$utvalgTxt
+
+N <- dim(RegData)[1]
+Ngrense <- 5		#Minste antall registreringer for at et sykehus skal bli vist
+if(N > 0) {Nsh <- table(RegData[ ,grVar])}	else {Nsh <- 0}
+
+Nshtxt <- paste(' (', as.character(Nsh),')', sep='') #paste('N=', as.character(Nsh), sep='')
+indShUt <- which(Nsh < Ngrense)
+if (length(indShUt)==0) { indShUt <- 0}
+Nshtxt[indShUt] <- paste(' (<', Ngrense,')',sep='')	#paste('N<', Ngrense,sep='')
+
+
+vt <- switch(valgtVar, Alder='alder', 
+					DagerRehab='antall dager med rehabilitering', 
+					DagerTilRehab='antall dager før rehabilitering', 
+					OpphTot= 'totalt opphold',
+					Permisjon = 'permisjonstid (for de som har hatt permisjon)')
+
+if (valgtMaal=='Med') {
+t1 <- 'Median ' } else {t1 <- 'Gjennomsnittlig '}
+
+tittel <- c(paste(t1, vt, sep=''),
+			'med 95% konfidensintervall')
+			
+#-----------Figur---------------------------------------
+if 	( max(Nsh) < Ngrense)	{#Dvs. hvis ALLE er mindre enn grensa.
+figtype(outfile)
+	plot.new()
+	if (dim(RegData)[1]>0) {
+	tekst <- paste('Færre enn ', Ngrense, ' registreringer ved hvert av sykehusene', sep='')
+	title(main=tittel, cex=0.95)
+	legend('topleft',utvalgTxt, bty='n', cex=0.9)
+	} else {
+	tekst <- 'Ingen registrerte data for dette utvalget' }
+	text(0.5, 0.5, tekst,cex=1)	#, family="sans")
+if ( outfile != '') {dev.off()}
+} else {
+#--------------------------------------------------------
+
+
+#Kommer ut ferdig sortert!
+if (valgtMaal=='Med') {
+	MedIQR <- plot(RegData[ ,grVar], RegData$Variabel, notch=TRUE, plot=FALSE)
+	MedIQR$stats[ ,indShUt] <- 0
+	MedIQR$conf[ ,indShUt] <- 0
+	sortInd <- order( MedIQR$stats[3,], decreasing=TRUE) 
+	Midt <- as.numeric(MedIQR$stats[3, sortInd])
+	KIned <- MedIQR$conf[1, sortInd]
+	KIopp <- MedIQR$conf[2, sortInd]
+#	MidtHele <- median(RegData$Variabel)
+	MedIQRHele <-  boxplot.stats(RegData$Variabel, do.conf = TRUE)
+	MidtHele <- as.numeric(MedIQRHele$stats[3])
+	KIHele <- MedIQRHele$conf
+	#Hvis vil bruke vanlige konf.int:
+	#j <- ceiling(N/2 - 1.96*sqrt(N/4))
+	#k <- ceiling(N/2 + 1.96*sqrt(N/4))
+	#KIHele <- sort(RegData$Variabel)[c(j,k)]
+#The notches (if requested) extend to +/-1.58 IQR/sqrt(n). (Chambers et al. (1983, p. 62), given in McGill et al. (1978, p. 16).) 
+#They are based on asymptotic normality of the median and roughly equal sample sizes for the two medians being compared, 
+#and are said to be rather insensitive to the underlying distributions of the samples. The idea appears to be to give 
+#roughly a 95% confidence interval for the difference in two medians. 	
+
+	
+} else {	#Gjennomsnitt blir standard.
+	Gjsn <- tapply(RegData$Variabel, RegData[ ,grVar], mean, na.rm=T)
+	SE <- tapply(RegData$Variabel, RegData[ ,grVar], sd, na.rm=T)/sqrt(Nsh)
+	Gjsn[indShUt] <- 0
+	SE[indShUt] <- 0
+	sortInd <- order(Gjsn, decreasing=TRUE) 
+	Midt <- as.numeric(Gjsn[sortInd])
+	KIned <- Gjsn[sortInd] - 2*SE[sortInd]
+	KIopp <- Gjsn[sortInd] + 2*SE[sortInd]
+	MidtHele <- mean(RegData$Variabel)
+	KIHele <- MidtHele + sd(RegData$Variabel)/sqrt(N)*c(-2,2)
+} 
+
+ShNavnSort <- paste(names(Nsh)[sortInd], Nshtxt[sortInd], sep='')
+plotdata <- c(Midt, KIned, KIopp)
+
+AntSh <- length(which(Midt>0))
+#soyletxt <- c(round(Midt[1:AntSh],1), rep('',length(Nsh)-AntSh))
+xmax <-  min(1.1*max(plotdata), 1.5*max(Midt))
+xlabt <- switch(valgtVar, Alder='alder (år)', 
+					DagerRehab='dager', 
+					DagerTilRehab='dager før rehabilitering', 
+					OpphTot= 'dager',
+					Permisjon = 'permisjonstid (dager)')
+
+
+#--------------------------FIGUR---------------------------------------------------
+FigTypUt <- figtype(outfile, fargepalett=Utvalg$fargepalett)
+farger <- FigTypUt$farger
+par('fig'=c(0.1, 1, 0, 0.91))
+	#plot.new()
+	pos <- barplot(Midt, horiz=T, border=NA, col=farger[3], #main=tittel,
+		xlim=c(0,xmax), ylim=c(0,3.9), 
+			font.main=1, xlab=paste(t1, vt, sep=''), las=1, cex.names=0.8) 	#xlim=c(0,ymax),
+	indShUtPlot <- AntSh+(1:length(indShUt)) 
+	posKI <- pos[1:AntSh]
+	ybunn <- 0.1
+	ytopp <- 3.7	#c(0, max(posKI) + min(posKI))
+	polygon( c(rep(KIHele[1],2), rep(KIHele[2],2)), c(ybunn, ytopp, ytopp, ybunn), 
+		col=farger[4], border=farger[4])
+	lines(x=rep(MidtHele, 2), y=c(ybunn, ytopp), col=farger[2], lwd=3) 
+	legend('topright', xjust=1, fill=c(farger[4],'white'), border=c(farger[4],'white'), cex=0.8, #lwd=2, #col=farger[2],
+		legend='95% konf.int., alle sykehus', bty='o', bg='white', box.col='white')
+	barplot(Midt, horiz=T, border=NA, col=farger[3], 	#main=tittel,
+		xlim=c(0, xmax), add=TRUE,
+			font.main=1, xlab=paste(t1, vt, sep=''), las=1, cex.names=0.5) 	#xlim=c(0,ymax), 
+	title(tittel, line=1, font.main=1, cex.main=1.2)
+
+#	text(x=0, y=pos+0.1, soyletxt, las=1, cex=0.8, adj=0, col=farger[1])
+	utvpos <- 6
+	mtext(utvalgTxt[1], side=3, col=farger[1], las=1, cex=0.9, adj=0, line=utvpos)
+	mtext(utvalgTxt[2], side=3, col=farger[1],las=1, cex=0.9, adj=0, line=utvpos-0.8)
+	mtext(utvalgTxt[3], side=3, col=farger[1],las=1, cex=0.9, adj=0, line=utvpos-1.6)
+	mtext(utvalgTxt[4], side=3, col=farger[1],las=1, cex=0.9, adj=0, line=utvpos-2.4)
+	mtext(utvalgTxt[5], side=3, col=farger[1],las=1, cex=0.9, adj=0, line=utvpos-3.2)
+	
+	mtext(at=pos+0.1, ShNavnSort, side=2, las=1, cex=0.9, adj=1, line=0.25)	#Hvis vil legge på navn som eget steg
+	options(warn = -1)	#Unngå melding om KI med lengde 0. Fungerer av en eller annen grunn ikke i pdf.
+	arrows(x0=Midt[-indShUtPlot]*0.999, y0=posKI, x1=KIopp[-indShUtPlot], y1=posKI, 
+		length=0.5/max(pos), code=2, angle=90, lwd=2, col=farger[1])
+	arrows(x0=Midt[-indShUtPlot]*1.001, y0=posKI, x1=KIned[-indShUtPlot], y1=posKI, 
+		length=0.5/max(pos), code=2, angle=90, lwd=2, col=farger[1])
+par('fig'=c(0, 1, 0, 1)) 
+if ( outfile != '') {dev.off()}
+#----------------------------------------------------------------------------------
+}
+}
