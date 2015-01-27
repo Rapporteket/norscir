b349d9fedb2e31ce608c252f8812db1e1d894043
areedv <biorakel@gmail.com>
2015-01-27 13:59:57 +0100

    roxygenize function and add rapbase function calls

--- R/NSFigAndelStabel.R
+++ R/NSFigAndelStabel.R
@@ -1,39 +1,38 @@
-FigAndelStabel <- function(RegData, outfile='', libkat, valgtVar, datoFra='2010-01-01', datoTil='3000-01-01', 
+#' Søylediagram som sammenligner fordeling (vha. stablede søyler) av valgt variabel
+#'
+#' Søylediagram som sammenligner fordeling (vha. stablede søyler) av valgt variabel
+#' hos fra to ulike utvalg, f.eks. eget/resten, før/etter
+#' (Kan velge å ikke ha med sammenligninga)
+#' Søylediagram som viser andeler av valgt variabel:
+#' NevrNivaaInn' - Nevrologisk nivå ved innskrivelse, sml. eget, andre
+#' NevrNivaaUt' - Nevrologisk nivå ved utskrivelse, sml. eget, andre
+#' NevrNivaaInnUt' - sml. Nevrologisk nivå ved inn- og utskrivelse
+#' Hvilke kombinasjonsmuligheter har vi?
+#' Inn / Ut (Velge: bare eget/hele landet)
+#' Eget / resten av landet (Velge: Inn eller Ut)
+#'
+#' @param RegData - ei dataramme med alle nødvendige variable fra registeret
+#' @param libkat - sti til bibliotekkatalog
+#' @param outfile - navn på fil figuren skrives ned til
+#' @param reshID - avdelingsid for egen avdeling, må angis
+#' Brukerstyrt i Jasper:
+#' @param valgtVar - Må velges: ... NevrNivaaInnUt, NevrNivaaInn, NevrNivaaUt
+#' @param egenavd - 1:eget sykehus, 0:hele landet (standard) Kun for valgtVar=='NevrNivaaInnUt'
+#' @param erMann - kjønn, 1-menn, 0-kvinner, standard: '' (alt annet enn 0 og 1), dvs. begge
+#' @param minald - alder, fra og med
+#' @param maxald - alder, til og med
+#' @param traume - 'ja','nei', standard: ikke valgt
+#' @param datoFra <- '2010-01-01'    # min og max dato i utvalget vises alltid i figuren.
+#' @param datoTil <- '2013-05-25'
+
+FigAndelStabel <- function(RegData, outfile='', libkat, valgtVar, datoFra='2010-01-01', datoTil='3000-01-01',
 		minald=0, maxald=120, erMann='', traume='', egenavd=1 , reshID)
 {
-#Søylediagram som sammenligner fordeling (vha. stablede søyler) av valgt variabel 
-#hos fra to ulike utvalg, f.eks. eget/resten, før/etter
-#(Kan velge å ikke ha med sammenligninga)
-
-#Søylediagram som viser andeler av valgt variabel: 
-#'NevrNivaaInn' - Nevrologisk nivå ved innskrivelse, sml. eget, andre
-#'NevrNivaaUt' - Nevrologisk nivå ved utskrivelse, sml. eget, andre
-#'NevrNivaaInnUt' - sml. Nevrologisk nivå ved inn- og utskrivelse
-
-#Hvilke kombinasjonsmuligheter har vi?
-#Inn / Ut (Velge: bare eget/hele landet)
-#Eget / resten av landet (Velge: Inn eller Ut)
 
 
-#Inngangsdata: 	
-#	RegData - ei dataramme med alle nødvendige variable fra registeret
-#	libkat - sti til bibliotekkatalog
-#   outfile - navn på fil figuren skrives ned til
-#	reshID - avdelingsid for egen avdeling, må angis
-# 	Brukerstyrt i Jasper:
-#		valgtVar - Må velges: ... NevrNivaaInnUt, NevrNivaaInn, NevrNivaaUt 
-#		egenavd - 1:eget sykehus, 0:hele landet (standard) Kun for valgtVar=='NevrNivaaInnUt'
-#		erMann - kjønn, 1-menn, 0-kvinner, standard: '' (alt annet enn 0 og 1), dvs. begge
-#		minald - alder, fra og med
-#		maxald - alder, til og med
-#		traume - 'ja','nei', standard: ikke valgt
-#		datoFra <- '2010-01-01'    # min og max dato i utvalget vises alltid i figuren.
-#		datoTil <- '2013-05-25'
-
-
-#Trenger funksjonene...:? 
-source(paste(libkat, 'LibFigFilType.R', sep=''), encoding="UTF-8")
-source(paste(libkat, 'NSLibUtvalg.R', sep=''), encoding="UTF-8")
+#Trenger funksjonene...:?, men ikke i pakken
+#source(paste(libkat, 'LibFigFilType.R', sep=''), encoding="UTF-8")
+#source(paste(libkat, 'NSLibUtvalg.R', sep=''), encoding="UTF-8")
 
 #------------Gjøre utvalg-------------------------
 #Definerer funksjonsspesifikke variable................
@@ -41,9 +40,9 @@ source(paste(libkat, 'NSLibUtvalg.R', sep=''), encoding="UTF-8")
 if (valgtVar %in% c('NevrNivaaInn','NevrNivaaUt')) {
 	RegData$Variabel <- as.numeric(RegData[ ,valgtVar])} else {
 	 RegData$Variabel <- RegData$NevrNivaaInn-RegData$NevrNivaaUt}	#Vil bare ha pasienter som har reg. både inn og ut.
-	
+
 #Gjør utvalg (Manglende data i variablene tas høyde for i variabeldef.)
-Utvalg <- NSLibUtvalg(RegData=RegData, datoFra=datoFra, datoTil=datoTil, minald=minald, maxald=maxald, 
+Utvalg <- NSLibUtvalg(RegData=RegData, datoFra=datoFra, datoTil=datoTil, minald=minald, maxald=maxald,
 		erMann=erMann, traume=traume, AIS='')
 RegData <- Utvalg$RegData
 utvalgTxt <- Utvalg$utvalgTxt
@@ -55,7 +54,7 @@ grtxt2 <- ''
 
 #Sml eget/resten av landet.
 if (valgtVar %in% c('NevrNivaaInn','NevrNivaaUt')) {
-#	if (reshID != 0) {ShNavn <- '' 
+#	if (reshID != 0) {ShNavn <- ''
 #					Andre <- 'Alle'}
 #	if (reshID %in% c(105593, 106896, 107627)) {
 	ShNavn <- as.character(RegData$ShNavn[which(RegData$ReshId == reshID)[1]])
@@ -69,7 +68,7 @@ if (valgtVar %in% c('NevrNivaaInn','NevrNivaaUt')) {
 	#Slå sm D og E
 	RegData <- RegData[RegData$AIS %in% LETTERS[1:5], ]	#Bare de med kategori A:E, ikke U
 	RegData$AIS[which(RegData$AIS %in% c('D', 'E'))] <- 'D+E'
-	ind <- list(Sh=which(RegData$ReshId == reshID), 
+	ind <- list(Sh=which(RegData$ReshId == reshID),
 				Rest=which(RegData$ReshId != reshID))
 	#Alternativt bruke to datasett?
 	RegDataLand <- RegData
@@ -130,9 +129,9 @@ if (valgtVar == 'NevrNivaaInnUt') {
 #------------------------- Beregninger -----------------------------------------
 #utvalg <- c('Sh', 'Rest') #som Var1 og Var 2
 #Andeler <- list(Sh = 0, Rest =0)
-#ind_sh <-which(as.numeric(RegData$AvdReshID)==egenReshID) 	
+#ind_sh <-which(as.numeric(RegData$AvdReshID)==egenReshID)
 #ind_resten <- which(as.numeric(RegData$AvdReshID)!=egenReshID)
-#Ngr <- cbind('Egen avdeling'=table(RegData[ind_sh, variabel]), 
+#Ngr <- cbind('Egen avdeling'=table(RegData[ind_sh, variabel]),
 #			'Landet forøvrig'= table(RegData[ind_resten, variabel]))
 N <- colSums(Ngr)
 AndelStabel <- round(prop.table(Ngr,2)*100, 1)
@@ -140,7 +139,7 @@ AndelStabel <- round(prop.table(Ngr,2)*100, 1)
 
 if (dim(RegData)[1]==0)	{# | (NSh==0 & egenavd==1)) {
 	#-----------Figur---------------------------------------
-figtype(outfile)
+rapbase::figtype(outfile)
 	plot.new()
 	title(main=tittel)
 	text(0.5, 0.7, 'Ingen egne data registrert for dette utvalget',cex=1.3)
@@ -151,7 +150,7 @@ figtype(outfile)
 #Inn parametre: subtxt, grtxt1, grtxt2, tittel, libkat, AndelStabel
 
 #Plottspesifikke parametre:
-FigTypUt <- figtype(outfile, fargepalett=Utvalg$fargepalett)
+FigTypUt <- rapbase::figtype(outfile, fargepalett=Utvalg$fargepalett)
 farger <- FigTypUt$farger
 
 par('fig'=c(0, 1, 0, 0.9))
@@ -164,8 +163,8 @@ if ((valgtVar %in% c('NevrNivaaInn','NevrNivaaUt')) & (reshID == 0)) {
 	text(pos, 102.6, paste0(c('','N='), N), cex=0.9)} else {
 	text(pos, 102.6, paste('N=', N, sep=''), cex=0.9)}	 #Antall over søyla
 mtext(at=pos, grtxt1, side=1, las=1, cex=0.8, adj=0.5, line=0.3)	#Tekst under søylene
-mtext(at=c(1, 4.1, 7.2), grtxt2, side=1, cex=0.9, adj=0.5, line=1.3)	
-mtext(subtxt, side=1, cex=1, adj=0.5, line=2.7)	
+mtext(at=c(1, 4.1, 7.2), grtxt2, side=1, cex=0.9, adj=0.5, line=1.3)
+mtext(subtxt, side=1, cex=1, adj=0.5, line=2.7)
 
 utvpos <- 5.2
 avst <- 0.8
@@ -177,7 +176,7 @@ mtext(utvalgTxt[3], side=3, las=1, cex=krymp, adj=0, line=utvpos-2*avst, col=far
 mtext(utvalgTxt[4], side=3, las=1, cex=krymp, adj=0, line=utvpos-3*avst, col=farger[1])
 mtext(utvalgTxt[5], side=3, las=1, cex=krymp, adj=0, line=utvpos-4*avst, col=farger[1])
 
-par('fig'=c(0, 1, 0, 1)) 
+par('fig'=c(0, 1, 0, 1))
 if ( outfile != '') {dev.off()}
 #------------------------------------------------------------------------------
 
