0264a0544693f06150146680a5336a234e629b01
areedv <biorakel@gmail.com>
2015-01-28 13:20:43 +0100

    add documentation of sample data

---
+++ R/NorScirSampleData.R
@@ -0,0 +1,11 @@
+#' Sample dataset for NorScir.
+#'
+#' A dataset containing a sample data with nonesense personal info
+#'
+#' @name NorScirSampleData
+#' @format A data frame with xxx rows and yy variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#' }
+NULL
